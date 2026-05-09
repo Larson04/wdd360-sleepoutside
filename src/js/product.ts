ec3c4e42b1c6f7a0d2e3c5e20e0047bb1b9ed77c
ameliarose003 <ami.halsey1@outlook.com>
2026-05-08 21:03:33 -0600

    add to cart button fixed

--- src/js/product.ts
+++ src/js/product.ts
@@ -1,15 +1,16 @@
 import type {Product} from "./types.mts"
-import { setLocalStorage } from "./utils.mts";
+import { updateCart } from "./utils.mts";
 import { findProductById } from "./productData.mts";
 
 function addProductToCart(product:Product) {
-  setLocalStorage("so-cart", product);
+  updateCart("so-cart", product);
 }
 // add to cart button event handler
 async function addToCartHandler(e:Event) {
   const target = e.target as HTMLButtonElement
   if(target.dataset.id) {
     const product = await findProductById(target.dataset.id);
+    console.log(product);
     addProductToCart(product);
   }
 }
--- src/js/utils.mts
+++ src/js/utils.mts
@@ -14,6 +14,14 @@ export function getLocalStorage(key:string) {
 export function setLocalStorage(key:string, data:any) {
   localStorage.setItem(key, JSON.stringify(data));
 }
+
+export function updateCart(key:string, data:any) {
+  const storedCart = getLocalStorage(key);
+  const cart = Array.isArray(storedCart) ? storedCart : [];
+  cart.push(data);
+  setLocalStorage(key, cart);
+}
+
 // set a listener for both touchend and click
 interface ClickHandler {
   (e:Event):void;
