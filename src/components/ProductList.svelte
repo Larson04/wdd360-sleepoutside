139bfd792d518afe1bc433f5e24b28db17f0cb2c
Sophie Archibald <arc21004@byui.edu>
2026-06-16 23:12:22 -0600

    Added quick view button

--- src/components/ProductList.svelte
+++ src/components/ProductList.svelte
@@ -4,9 +4,11 @@
   import { getProducts } from "../js/productData.mjs";
   import type { Product } from "../js/types.mts";
   import ProductSummary from "./ProductSummary.svelte";
+  import QuickViewModal from "./QuickViewModal.svelte";
   
   let category = $state(""); 
   let products:Product[] = $state([]);
+  let selectedProduct:Product | null = $state(null);
 
   async function init() {
     category = getParam("category") || ""
@@ -17,11 +19,14 @@
   onMount(init);
 </script>
 
-
 <h2>Top products: {category}</h2>
 
 <ul class="product-list">
   {#each products as product}
-    <ProductSummary {product} /> 
+    <ProductSummary {product} onQuickView={(product) => selectedProduct = product}/> 
   {/each}
-  </ul>
+</ul>
+
+{#if selectedProduct} 
+<QuickViewModal product={selectedProduct} onClose={() => selectedProduct = null}/>
+{/if}
